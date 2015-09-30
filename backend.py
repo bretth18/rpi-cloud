2b29b86f0d1902a421c63211c97355073716f644
Brett Henderson <bretth18@gmail.com>
2015-09-30 12:22:50 -0700

    backend.py updated
    big booty

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 import logging
-import absolute_import
-import unicode_literals
+##import absolute_import
+##import unicode_literals
 
 logger = logging.getLogger(__name__)
 
@@ -94,7 +94,7 @@ class PlaybackProvider(object):
     def get_time_position(self):
         return self.audio.get_position().get()
 
-class PlaylistProver(object)
+class PlaylistProver(object):
 
     pykka_traversable = True
 
@@ -109,3 +109,22 @@ class PlaylistProver(object)
 
     def create(self, name):
         raise NotImplementedError
+
+    def delete(self, uri):
+        raise NotImplementedError
+
+    def lookup(self, uri):
+        raise NotImplementedError
+
+    def refresh(self):
+        raise NotImplementedError
+
+    def save(self, playlists):
+        raise NotImplementedError
+
+class BacksideListener(listener.Listener):
+    @staticmethod
+    def send(event, **kwargs):
+        listener.send(BacksideListener, event, **kwargs)
+    def playlists_loaded(self):
+        pass
